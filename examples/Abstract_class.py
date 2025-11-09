173e4732c5782f2beb9ce45e04f955bc7da13605
mazy99 <mazy.1999@mail.ru>
2025-11-09 17:05:00 +0300

    fist commit

---
+++ examples/Abstract_class.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from abc import ABC, abstractmethod
+
+
+class Polygon(ABC):
+
+    @abstractmethod
+    def noofsides(self):
+        pass
+
+
+class Triangle(Polygon):
+
+    def noofsides(self):
+        print("I have 3 sides")
+
+
+class Pentagon(Polygon):
+
+    def noofsides(self):
+        print("I have 5 sides")
+
+
+class Hexagon(Polygon):
+
+    def noofsides(self):
+        print("I have 6 sides")
+
+
+class Quadrilateral(Polygon):
+
+    def noofsides(self):
+        print("I have 4 sides")
+
+
+R = Triangle()
+R.noofsides()
+
+K = Quadrilateral()
+K.noofsides()
+
+P = Pentagon()
+P.noofsides()
+
+H = Hexagon()
+H.noofsides()
